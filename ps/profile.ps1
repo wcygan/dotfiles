21398a7ae52abc26cc0145e263f8c918631e7d23
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-09-03 16:14:39 -0700

    added powershell profile

---
+++ ps/profile.ps1
@@ -0,0 +1,78 @@
+# Guide to setting this up: 
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.1#how-to-create-a-profile
+
+Set-Location C:\Users\Will
+
+function pro {code $PROFILE}
+
+function dev { Set-Location C:\Users\Will\Development }
+
+function sem { Set-Location C:\Users\Will\School }
+function will { Set-Location C:\Users\Will }
+
+function .. {
+    Set-Location ..
+}
+
+function ... {
+    ..
+    ..
+}
+
+function .... {
+    ...
+    ..
+}
+
+function Get-Git-Status {
+    git status
+}
+
+function Get-Git-Log {
+    git log
+}
+
+function Set-Git-Commit {
+    git commit $args
+}
+function Set-Git-Clone {
+    git clone $args
+}
+
+function Set-Git-Pull-Rebase {
+    git pull --rebase
+}
+
+function Set-Git-Add-All {
+    git add .
+}
+
+
+function Get-Cargo-Build {
+    cargo build
+}
+
+function Get-Cargo-Test {
+    cargo test
+}
+
+function Get-Cargo-Run {
+    cargo run
+}
+
+function Get-Cargo-Check {
+    cargo check
+}
+
+Set-Alias -Name l -Value "ls"
+Set-Alias -Name c -Value "clear"
+Set-Alias -Name gaa -Value Set-Git-Add-All
+Set-Alias -Name gs -Value Get-Git-Status
+Set-Alias -Name gitc -Value Set-Git-Commit
+Set-Alias -Name glog -Value  Get-Git-Log
+Set-Alias -Name gcl -Value Set-Git-Clone
+Set-Alias -Name gpr -Value Set-Git-Pull-Rebase
+Set-Alias -Name cgc -Value Get-Cargo-Check
+Set-Alias -Name cgr -Value Get-Cargo-Run
+Set-Alias -Name cgb -Value Get-Cargo-Build
+Set-Alias -Name cgt -Value Get-Cargo-Test

7acb1369d26063338f1f54d940c1576078de76a6
Will Cygan <wcygan.io@gmail.com>
2024-08-26 11:36:13 +0200

    making the settings and keybinds easier to use

---
+++ zed/apply.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+settings=~/.config/zed/settings.json
+keymap=~/.config/zed/keymap.json
+
+# TODO: copy /zed/keymap.json to ~/.config/zed/keymap.json if it exists
+# TODO: copy /zed/settings.json to ~/.config/zed/settings.json if it exists
